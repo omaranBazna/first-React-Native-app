ea0a07c62940353b3604219447e6d9fc04600809
omaran bazna <omran4d@gmail.com>
2022-08-25 04:04:01 -0400

    add content to the Directory Screen Component

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -1,2 +1,14 @@
 import { FlatList } from "react-native";
 import { Avatar, ListItem } from "react-native-elements";
+
+const DirectoryScreen = (props) => {
+  return (
+    <FlatList
+      data={props.campsites}
+      renderItem={renderDirectoryItem}
+      keyExtractor={(item) => item.id.toString()}
+    />
+  );
+};
+
+export default DirectoryScreen;
