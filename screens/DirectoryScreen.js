e56815d39e583fd61e1b3d45a562ce1e77561cb6
omaran bazna <omran4d@gmail.com>
2022-08-25 04:07:37 -0400

    finish Directory screen

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -2,6 +2,17 @@ import { FlatList } from "react-native";
 import { Avatar, ListItem } from "react-native-elements";
 
 const DirectoryScreen = (props) => {
+  const renderDirectoryItem = ({ item: campsite }) => {
+    return (
+      <ListItem>
+        <Avatar source={campsite.image} rounded />
+        <ListItem.Content>
+          <ListItem.Title>{campsite.name}</ListItem.Title>
+          <ListItem.Subtitle>{campsite.description}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    );
+  };
   return (
     <FlatList
       data={props.campsites}
